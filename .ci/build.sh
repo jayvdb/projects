6dbf7b5759d5e4c532790cf77e656eb0191741e1
John Vandenberg <jayvdb@gmail.com>
2017-11-10 14:46:25 +0700

    Show value of PULL_REQUEST

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
+if [ -n "$PULL_REQUEST" ]; then
+  echo PULL_REQUEST is $PULL_REQUEST
+else
+  export JEKYLL_ENV=production
+fi
+
 bundle exec jekyll build
 bundle exec htmlproofer ./_site \
 	--disable-external \
