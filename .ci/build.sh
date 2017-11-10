9fd1a1ac0ba7b1734acfe6f8f38d39439f2528bc
John Vandenberg <jayvdb@gmail.com>
2017-11-10 15:14:49 +0700

    More vars

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-if [ -n "$PULL_REQUEST" ]; then
+echo REPOSITORY_URL=$REPOSITORY_URL
+echo BRANCH=$BRANCH
+echo HEAD=$HEAD
+echo PULL_REQUEST=$PULL_REQUEST
+echo COMMIT_REF=$COMMIT_REF
+echo CONTEXT=$CONTEXT
+
+if [[ -n "$PULL_REQUEST" && "$PULL_REQUEST" != "false" ]]; then
   echo PULL_REQUEST is $PULL_REQUEST
 else
   export JEKYLL_ENV=production
