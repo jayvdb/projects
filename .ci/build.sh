a9c4a5a914d2487f66fc6730bb2262219231b3a4
John Vandenberg <jayvdb@gmail.com>
2017-11-10 15:27:37 +0700

    Only master is production

--- .ci/build.sh
+++ .ci/build.sh
@@ -9,12 +9,18 @@ echo COMMIT_REF=$COMMIT_REF
 echo CONTEXT=$CONTEXT
 
 if [[ -n "$DEPLOY_URL" && "${DEPLOY_URL/netlify//}" != "$DEPLOY_URL" ]]; then
+  CI=netlify
   rvm use ruby 2.2.3
 fi
 
-if [[ -n "$PULL_REQUEST" && "$PULL_REQUEST" != "false" ]]; then
-  echo PULL_REQUEST is $PULL_REQUEST
-else
+# Netlify builds are deploys which can be previewed,
+# and need to have a different UI except on master.
+if [[ "$CI" == netlify && -z "$JEKYLL_ENV" ]]; then
+  if [[ "$BRANCH" == master ]]; then
+    export JEKYLL_ENV=production
+  fi
+elif [[ -z "$JEKYLL_ENV" ]]; then
+  # Default to production builds
   export JEKYLL_ENV=production
 fi
 
